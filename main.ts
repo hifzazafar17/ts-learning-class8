413960ce87f9b10dab22687ddfbf2965b279837e
Hifza Zafar <152292315+Hifza-Code-Journey@users.noreply.github.com>
2024-02-22 22:54:35 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,51 @@
+// // object 
+// type Car ={
+//     model:number;         
+//     color:string;
+// }
+
+// type Person={
+//     name: string;
+//     age: number;
+//     email: string;
+//     sister:{
+//         name:string;
+//         age:number;               
+//     },
+//     car?: Car
+// }
+
+
+// // nested Object
+// const person: Person = {
+//     name: "hifza",
+//     age: 23,
+//     email: "hifzazafar116@getDefaultAutoSelectFamily.com",
+//     sister: {
+//         name: "hareem",
+//         age: 18,
+//     },
+//     car:{
+//         model:2022,
+//         color:"black"
+//     },
+// }
+
+
+// const person2: Person = {
+//     name: "haniya",
+//     age: 20,
+//     email: "hifza@gmail.com"
+// }
+
+// const car:Car={
+//     model:2022,
+//     color:"white",
+// }
+
+// let a: string = "hareem";
+// let b: unknown = 2;
+
+// a = b as number;
+
+
